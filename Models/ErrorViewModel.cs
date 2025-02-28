cf305b739ea7fd6e820674ec2c873b896f4a92c7
22SOECE11080 <135407619+22SOECE11080@users.noreply.github.com>
2025-02-28 10:29:49 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace apartment_management_system_asp_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
